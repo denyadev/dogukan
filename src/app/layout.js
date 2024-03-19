3f199f2d3f9ce21f9d8bcbfd6047c6b0f9ab1b8e
Denis Kosogov <denis.kosogov@gmail.com>
2024-03-19 14:22:26 -0400

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,22 +1,38 @@
 import Header from "@/components/Header";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-    title: "Adasa Moving and Transportation",
-    description:
-        "Adasa Moving and Transportation, a recently established family-owned and operated business, proudly presents a rich 15-year history specializing in local moving services across Toronto and the Greater Toronto Area (GTA)",
+  title: "Adasa Moving and Transportation",
+  description:
+    "Adasa Moving and Transportation, a recently established family-owned and operated business, proudly presents a rich 15-year history specializing in local moving services across Toronto and the Greater Toronto Area (GTA)",
 };
 
 export default function RootLayout({ children }) {
-    return (
-        <html lang="en">
-            <body className={inter.className}>
-                <Header />
-                {children}
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <head>
+        <Script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+
+        <Script id="google-analytics">
+          {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+  `}
+        </Script>
+      </head>
+      <body className={inter.className}>
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -5,42 +5,21 @@ import Hero from "@/components/Hero";
 import Services from "@/components/Services";
 import Testimonials from "@/components/Testimonials";
 import WhyUs from "@/components/WhyUs";
-import Image from "next/image";
 import Footer from "@/components/Footer";
-import Script from "next/script";
 
 export default function Home() {
-    return (
-        <>
-            {/* Load the gtag.js library */}
-            <Script
-                src="https://www.googletagmanager.com/gtag/js?id=AW-11467367917"
-                strategy="afterInteractive"
-                async
-            />
-            {/* Initialize and configure gtag.js */}
-            <Script
-                id="google-analytics"
-                strategy="afterInteractive"
-                dangerouslySetInnerHTML={{
-                    __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'AW-11467367917');
-          `,
-                }}
-            />
-            <section>
-                <Hero />
-                <Bar />
-                <About />
-                <Services />
-                <WhyUs />
-                <Testimonials />
-                <Contact />
-                <Footer />
-            </section>
-        </>
-    );
+  return (
+    <>
+      <section>
+        <Hero />
+        <Bar />
+        <About />
+        <Services />
+        <WhyUs />
+        <Testimonials />
+        <Contact />
+        <Footer />
+      </section>
+    </>
+  );
 }

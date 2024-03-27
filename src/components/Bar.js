e753dd77296a438c31df68ed2aad62c8dda83a2a
Denis Kosogov <denis.kosogov@gmail.com>
2024-03-27 12:15:00 -0400

    update

--- src/components/Bar.js
+++ src/components/Bar.js
@@ -1,25 +1,23 @@
 export default function Bar() {
-    return (
-        <div className="flex w-full bg-[#FF6600] text-white uppercase text-sm py-4 overflow-hidden font-bold">
-            <div className="sponsor-animate whitespace-nowrap">
-                <div className="whitespace-nowrap animate-marquee">
-                    <span className="mx-16">
-                        CALL TODAY FOR AN ESTIMATE 647-648-0454
-                    </span>
-                    <span className="mx-16">60¢/pound ONTARIO - QUEBEC</span>
-                    <span className="mx-16">
-                        ALL CUSTOMERS ARE WELCOME TO COME AND SEE SCALE
-                    </span>
-                    <span className="mx-16">NO HIDDEN FEES</span>
-                    <span className="mx-16">
+  return (
+    <div className="flex w-full bg-[#FF6600] text-white uppercase text-sm py-4 overflow-hidden font-bold">
+      <div className="sponsor-animate whitespace-nowrap">
+        <div className="whitespace-nowrap animate-marquee">
+          <span className="mx-16">CALL TODAY FOR AN ESTIMATE 647-648-0454</span>
+          <span className="mx-16">60¢/pound ONTARIO - QUEBEC</span>
+          <span className="mx-16">
+            ALL CUSTOMERS ARE WELCOME TO COME AND SEE SCALE
+          </span>
+          <span className="mx-16">NO HIDDEN FEES</span>
+          {/* <span className="mx-16">
                         CERTIFIED BY CANADIAN ASSOCIATION OF MOVERS
-                    </span>
-                    <span className="mx-16">Discount for Students</span>
-                    <span className="mx-16">
-                        Every Monday - Tuesday Piano Moving Specials
-                    </span>
-                </div>
-            </div>
+                    </span> */}
+          <span className="mx-16">Discount for Students</span>
+          <span className="mx-16">
+            Every Monday - Tuesday Piano Moving Specials
+          </span>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
